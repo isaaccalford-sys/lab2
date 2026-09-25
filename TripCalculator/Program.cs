c86c0dd117b4e16dbd2578128bdb29688cbf7e3d
isaac alford <isaac.c.alford@gmail.com>
2026-09-25 13:17:22 -0400

    part 3 (pay check)

--- TripCalculator/Program.cs
+++ TripCalculator/Program.cs
@@ -1,5 +1,5 @@
 ﻿//part 1: Road Trip
-/*
+
 Console.Write("How many miles are you driving ");
 double tripMiles = Convert.ToDouble(Console.ReadLine());
 
@@ -18,8 +18,8 @@ double fuelCost = gallonsNeeded / pricePerGallon;
 
 System.Console.WriteLine("Gallons needed: " + gallonsNeeded.ToString("F2"));
 System.Console.WriteLine("Fuel cost " + fuelCost.ToString("C") );
-*/
-//part2
+
+//part: 2 pizza
  const int amountOfSlices = 8;
 
  System.Console.Write("How many people came to your party? ");
@@ -41,3 +41,23 @@ double pizzaCost =amountOfPizzas * pricePerPizza;
 System.Console.WriteLine("Total slices: " + totalSlices.ToString("F2"));
 System.Console.WriteLine("Slices per person: " + slicesPerPerson.ToString("F1"));
 System.Console.WriteLine("Pizza cost: " + pizzaCost.ToString("C"));
+
+//part 3: paycheck
+const Decimal taxRate = .18m;
+
+System.Console.Write("How many hours did you work this week? ");
+Decimal hoursWorked = Convert.ToDecimal(Console.ReadLine());
+
+System.Console.Write("What is you hourly rate? ");
+Decimal hourlyRate = Convert.ToDecimal(Console.ReadLine());
+
+
+Decimal grossPay = hoursWorked * hourlyRate;
+Decimal taxWithheld = grossPay * taxRate;
+Decimal takeHomePay = grossPay - taxWithheld;
+
+
+System.Console.WriteLine("Gross pay: " + grossPay.ToString("C"));
+System.Console.WriteLine("Tax withheld: " + taxWithheld.ToString("C"));
+System.Console.WriteLine("Take home pay: " + takeHomePay.ToString("C"));
+
